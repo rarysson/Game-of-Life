cb09b3b9ae958cdddf3be5cbaacfa1166b09c52b
Rarysson Guilherme <r_guilherme12@hotmail.com>
2024-01-05 17:35:19 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Game of Life
+
+A study about Rust and Bevy.
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, hello_world)
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello World");
+}
